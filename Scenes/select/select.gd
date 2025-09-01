1a5d1d8c207285e287d6045f028ec8d4538b6594
Riyansu <kenKEN.DEM@gmail.com>
2025-09-01 23:25:51 +0900

    process 2

--- Scenes/select/select.gd
+++ Scenes/select/select.gd
@@ -4,17 +4,17 @@ func _on_button_1_button_down():
 	Global.selected = 1
 	start()
 
-
-
 func _on_button_2_button_down():
 	Global.selected = 2
 	start()
 
-
 func _on_button_3_button_down():
 	Global.selected = 3
 	start()
-	
+
+func _on_button_4_button_down():
+	get_tree().change_scene_to_file("res://Scenes/wii/wii.tscn")
+
 func start():
 	print(Global.selected)
 	get_tree().change_scene_to_file("res://Scenes/launcher/launcher.tscn")
